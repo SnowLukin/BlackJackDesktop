38ea6aaf01bec6e82bc5bb2843823beeee617fac
Snow Lukin <69481493+SnowLukin@users.noreply.github.com>
2023-10-19 21:46:43 +0300

    Initial Commit

---
+++ BlackJackDesktop/BlackJackDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJackDesktopApp.swift
+//  BlackJackDesktop
+//
+//  Created by Snow Lukin on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BlackJackDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
